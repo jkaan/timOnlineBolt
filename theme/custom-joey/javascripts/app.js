13085575dea5defb0e7e9934464d4ede971e3b6b
Joey Kaan <joey94jk@gmail.com>
2015-07-09 18:27:28 +0200

    - Initial setup of Bolt with own built theme 'custom-joey' based on theme base-2014

---
+++ theme/custom-joey/javascripts/app.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    $(".button-collapse").sideNav();
+});
+
+// Initialize the Magnific popup shizzle. Fancybox is still here as a trigger, for backwards compatibility.
+$('.magnific, .fancybox, div.imageholder a').magnificPopup({
+    type: 'image',
+    gallery: { enabled: true },
+    disableOn: 400,
+    closeBtnInside: true,
+    enableEscapeKey: true,
+    mainClass: 'mfp-with-zoom',
+    zoom: {
+        enabled: true,
+        duration: 300,
+        easing: 'ease-in-out'
+    }
+});
+
+// Don't break on browsers without console.log();
+try { console.assert(1); } catch(e) { console = { log: function() {}, assert: function() {} } }
+
+
